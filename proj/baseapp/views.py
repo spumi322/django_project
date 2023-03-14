ab10e6084ed44e271cbf2f5269f1bb78b52aa162
spumi322 <martin.lee.white@gmail.com>
2023-03-14 15:09:56 +0100

    add database

--- proj/baseapp/views.py
+++ proj/baseapp/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Lets Learn Python!"},
-    {"id": 2, "name": "Lets Learn Javascript!"},
-    {"id": 3, "name": "Lets Learn C#!"},
-]
+# rooms = [
+#     {"id": 1, "name": "Lets Learn Python!"},
+#     {"id": 2, "name": "Lets Learn Javascript!"},
+#     {"id": 3, "name": "Lets Learn C#!"},
+# ]
 
 
 def home(request):
-    return render(request, "baseapp/home.html", {"rooms": rooms})
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+
+    return render(request, "baseapp/home.html", context)
 
 def room(request, pk):
-    return render(request, "baseapp/room.html")
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
+
+    return render(request, "baseapp/room.html", context)
+
+def createRoom(request):
+    context = {}
+    return render(request, "baseapp/room_form.html", context)
